b8ee91179d03a4832f666cdfcd38dd51142fae98
daniel24jan <daniel.januar@ui.ac.id>
2014-05-13 16:45:27 +0700

    Details Pelamar (belom disinkronin)

---
+++ SIOUIbackend/src/java/object/Pelamar.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package object;
+
+/**
+ *
+ * @author daniel.januar
+ */
+public class Pelamar {
+    
+    private int id;
+    private int id_lowongan;
+    private String username;
+    private String created_at;
+    private String updated_at;
+
+    public Pelamar(int id, int id_lowongan, String username, String created_at, String updated_at) {
+        this.id = id;
+        this.id_lowongan = id_lowongan;
+        this.username = username;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getId_lowongan() {
+        return id_lowongan;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setId_lowongan(int id_lowongan) {
+        this.id_lowongan = id_lowongan;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+}
